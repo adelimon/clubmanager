7c820c8e48ca97fec42124e88becf74a8122c4f1
Alan Delimon <adelimon@gmail.com>
2016-04-07 23:08:24 -0400

    https://github.com/adelimon/clubmanager/issues/8
    Only allow Users to view members that want to be viewed online.  Admins can see everyone.

--- src/main/java/com/ajdconsulting/pra/clubmanager/repository/MemberRepository.java
+++ src/main/java/com/ajdconsulting/pra/clubmanager/repository/MemberRepository.java
@@ -2,6 +2,8 @@ package com.ajdconsulting.pra.clubmanager.repository;
 
 import com.ajdconsulting.pra.clubmanager.domain.Member;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
 
 import java.util.List;
@@ -11,4 +13,6 @@ import java.util.List;
  */
 public interface MemberRepository extends JpaRepository<Member,Long> {
 
+    @Query
+    public Page<Member> findByViewOnline(Pageable pageable, boolean viewOnline);
 }
