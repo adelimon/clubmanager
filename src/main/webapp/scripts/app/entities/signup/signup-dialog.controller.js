8084537a01c61ac22b5b257adb745eb756c05b88
Alan Delimon <adelimon@gmail.com>
2016-04-13 23:13:53 -0400

    https://github.com/adelimon/clubmanager/issues/5
    Only allow users to signup for jobs that aren't taken for the current work day selected.  Also improved UI so that the job's point value and date's event type display in the dropdown.

--- src/main/webapp/scripts/app/entities/signup/signup-dialog.controller.js
+++ src/main/webapp/scripts/app/entities/signup/signup-dialog.controller.js
@@ -44,16 +44,35 @@ angular.module('clubmanagerApp').controller('SignupDialogController',
             var dateSelected = scheduleDate.date;
             // get all jobs
             var allJobs = $scope.allJobs;
+            var allSignups = $scope.allSignups;
 
             // first load job types for the user selected date.  We do this by getting the event type and matching
             // it against the job type.
             var eventTypeId = scheduleDate.eventType.id;
+            var eventId = scheduleDate.id;
 
+            // This is kind of a hack to get this working without mucking with the back end too much because I
+            // can't figure out how get all this angular sh1t to work with the Java back end in time so I did this.
+            // Basically we grab ALL the signups, then filter on the ones for the given date.  Then we go through
+            // all the jobs and only add the ones that are for the event type and not already taken.  Eventually
+            // I will move this to the back end, since the performance of this isn't going to be wonderful.  But it
+            // did only take an hour to write. :)
+            var dateSignups = new Array();
+            for (var index = 0; index < allSignups.length; index++) {
+                var signup = allSignups[index];
+                if (signup.scheduleDate.id == eventId) {
+                    dateSignups.push(signup.job.id);
+                }
+            }
+
+            var jobIdsForDate = dateSignups.join("#");
             for (var index = 0; index < allJobs.length; index++) {
                 console.log("a job is " + JSON.stringify(allJobs[index]));
                 var job = allJobs[index];
                 var isEventTypeJob = (eventTypeId === job.eventType.id);
-                if (isEventTypeJob) {
+                var isTaken = (jobIdsForDate.indexOf(job.id) >= 0);
+                // if the job's event type matches mine, then add the job to the list
+                if (isEventTypeJob && !isTaken) {
                     $scope.jobs.push(job);
                 }
             }
