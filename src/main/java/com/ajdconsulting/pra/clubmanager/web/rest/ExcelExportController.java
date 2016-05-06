c2dfa40e9e489ab94376fa7c794ccb9ee7d1e89f
Delimon, Alan <adelimon@paychex.com>
2016-05-06 15:01:06 -0400

    #19 - excel export with formatting and styles and printing boundaries set up.  The only thing left do is clean up this code since it is a bit of a mess right now, then I can close this story and deploy it to prod.

--- src/main/java/com/ajdconsulting/pra/clubmanager/web/rest/ExcelExportController.java
+++ src/main/java/com/ajdconsulting/pra/clubmanager/web/rest/ExcelExportController.java
@@ -1,39 +1,29 @@
 package com.ajdconsulting.pra.clubmanager.web.rest;
 
-import com.ajdconsulting.pra.clubmanager.domain.Signup;
-import com.ajdconsulting.pra.clubmanager.repository.SignupRepository;
 import com.mysql.jdbc.Driver;
-import org.apache.poi.hssf.usermodel.HSSFHeader;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFShape;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.poi.ss.usermodel.Font;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
-/**
- * <DESCRIBE WHAT YOUR CLASS DOES HERE AND DO NOT CHECK IN WITHOUT DOING THIS>
- *
- * @author adelimon
- * @date 5/3/2016
- * @since Advanced Security R2 Sprint 8
- */
 @Controller
 public class ExcelExportController {
 
+    private static final String DEFAULT_FILE_NAME = "signups.xlsx";
+
     @RequestMapping("/exportSignups")
     public void getExcel(HttpServletRequest request, HttpServletResponse response) throws IOException, SQLException {
+
         // 1. Fetch your data
         // 2. Create your excel
         // 3. write excel file to your response.
@@ -42,36 +32,71 @@ public class ExcelExportController {
         dataSource.setUrl("jdbc:mysql://localhost/clubmanager");
         dataSource.setUsername("root");
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-        List<Map<String, Object>> maps = jdbcTemplate.queryForList(
-                "select " +
-                        "w.first_name, w.last_name, j.title, j.point_value, j.cash_value, j.reserved, j.job_day, wl.last_name leader, sd.date " +
-                        "from  " +
-                        "job j " +
-                        "inner join schedule_date sd on sd.event_type_id = j.event_type_id " +
-                        "left join signup s on s.job_id = j.id " +
-                        "left join member w on w.id = s.worker_id " +
-                        "left join member  wl on wl.id = j.work_leader_id " +
-                        "where sd.date <= '2016-05-22' " +
-                        "order by sd.date, j.title"
-        );
-        HSSFWorkbook workbook = new HSSFWorkbook();
-        HSSFSheet signupSheet = workbook.createSheet("Signups");
 
+        // hacky way of escaping this input against potential mucking around and attempts at Sea Quill
+        // hot beef injections
+        String raceWeek = "'"+request.getParameter("raceWeekDate")+"' ";
+
+        List<Map<String, Object>> maps = jdbcTemplate.queryForList(
+            "select " +
+            "concat(w.first_name,' ', w.last_name) name, j.title, j.point_value, j.cash_value, j.reserved, j.job_day, wl.last_name leader, sd.date " +
+            "from  " +
+            "job j " +
+            "inner join schedule_date sd on sd.event_type_id = j.event_type_id " +
+            "left join signup s on s.job_id = j.id " +
+            "left join member w on w.id = s.worker_id " +
+            "left join member  wl on wl.id = j.work_leader_id " +
+            "where sd.date <= " + raceWeek +
+            "order by sd.date, j.title"
+        );
+        Workbook workbook = new XSSFWorkbook();
+        Sheet signupSheet = workbook.createSheet("Signups");
+        signupSheet.setDefaultRowHeightInPoints((short) 40);
+        signupSheet.setMargin(Sheet.LeftMargin, 0.25);
+        signupSheet.setMargin(Sheet.RightMargin, 0.25);
+        signupSheet.setMargin(Sheet.TopMargin, 0.25);
+        signupSheet.setMargin(Sheet.BottomMargin, 0.25);
         int record = 0;
         for (Map<String, Object> row : maps) {
-            HSSFRow excelRow = signupSheet.createRow(record++);
-            int rowIndex = 0;
+            Row excelRow = signupSheet.createRow(record++);
+            int columnIndex = 0;
             for (String key : row.keySet()) {
+                boolean reserved = Boolean.parseBoolean(row.getOrDefault("reserved", "false").toString());
                 String value = "";
                 if (row.get(key) != null) {
                     value = row.get(key).toString();
                 }
-                excelRow.createCell(rowIndex).setCellValue(value);
-                rowIndex++;
+                Cell cell = excelRow.createCell(columnIndex);
+                cell.setCellValue(value);
+
+                CellStyle style = workbook.createCellStyle();
+                if (reserved) {
+                    Font font = workbook.createFont();
+                    font.setBoldweight(Font.BOLDWEIGHT_BOLD);
+                    style.setFont(font);
+                }
+                IndexedColors fillColor = IndexedColors.WHITE;
+                if ((record % 2) == 0) {
+                    fillColor = IndexedColors.GREY_25_PERCENT;
+                }
+                style.setFillForegroundColor(fillColor.getIndex());
+           	    style.setFillPattern(CellStyle.SOLID_FOREGROUND);
+                style.setBorderTop(CellStyle.BORDER_THIN);
+                style.setBorderBottom(CellStyle.BORDER_THIN);
+                style.setBorderLeft(CellStyle.BORDER_THIN);
+                style.setBorderRight(CellStyle.BORDER_THIN);
+                cell.setCellStyle(style);
+                signupSheet.autoSizeColumn(columnIndex);
+                columnIndex++;
             }
         }
-        response.setContentType("application/vnd.ms-excel");
-        response.addHeader("content-disposition", "filename=signups.xls");
+        writeExcelToResponse(response, workbook);
+    }
+
+    private void writeExcelToResponse(HttpServletResponse response, Workbook workbook) throws IOException {
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        response.addHeader("content-disposition", "filename=" + DEFAULT_FILE_NAME);
         workbook.write(response.getOutputStream());
     }
+
 }
