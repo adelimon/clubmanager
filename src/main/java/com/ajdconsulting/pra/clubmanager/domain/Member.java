8fbfc993d01a990a542042f624f8819ae4f069fa
Alan Delimon <adelimon@gmail.com>
2016-06-02 16:27:11 -0400

    #55 - add status field to the member details screen

--- src/main/java/com/ajdconsulting/pra/clubmanager/domain/Member.java
+++ src/main/java/com/ajdconsulting/pra/clubmanager/domain/Member.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -72,8 +74,9 @@ public class Member implements Serializable {
     @Column(name = "current_year_points")
     private Float currentYearPoints;
 
-    @Column(name = "status")
-    private Long status;
+    @ManyToOne
+    @JoinColumn(name = "status")
+    private MemberTypes status;
 
     public Long getId() {
         return id;
@@ -191,6 +194,14 @@ public class Member implements Serializable {
         this.currentYearPoints = currentYearPoints;
     }
 
+    public MemberTypes getStatus() {
+        return status;
+    }
+
+    public void setStatus(MemberTypes status) {
+        this.status = status;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -228,13 +239,5 @@ public class Member implements Serializable {
             ", birthday='" + birthday + "'" +
             ", dateJoined='" + dateJoined + "'" +
             '}';
-    }
-
-    public Long getStatus() {
-        return status;
-    }
-
-    public void setStatus(Long status) {
-        this.status = status;
     }
 }
