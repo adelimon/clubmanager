9d7614bba9795d03fac85b9d090e47b0c4e6bb35
Delimon, Alan <adelimon@paychex.com>
2017-03-28 16:11:31 -0400

    #104 - points handling based on "fiscal" year.

---
+++ src/main/java/com/ajdconsulting/pra/clubmanager/dates/CurrentFiscalYear.java
@@ -0,0 +1,32 @@
+package com.ajdconsulting.pra.clubmanager.dates;
+
+import java.time.Month;
+import java.time.ZonedDateTime;
+
+/**
+ * Determine the current "fiscal" year for PRA.  This goes March to March.  So for points and work tracking purposes,
+ * fiscal 2017 would start in March 2017 and end on 2/28/18.
+ *
+ * This class encapsulates that logic so it is centralized.
+ *
+ */
+public final class CurrentFiscalYear {
+
+    private int fiscalYear;
+
+    private CurrentFiscalYear() {
+        ZonedDateTime now = ZonedDateTime.now();
+        // this syntax sucks.  we need operator overloading in Java!
+        boolean afterMarch = (now.getMonth().compareTo(Month.MARCH) >= 1);
+        if (afterMarch) {
+            fiscalYear = now.getYear();
+        } else {
+            fiscalYear = now.getYear()-1;
+        }
+    }
+
+    public static int getFiscalYear() {
+        CurrentFiscalYear year = new CurrentFiscalYear();
+        return year.fiscalYear;
+    }
+}
