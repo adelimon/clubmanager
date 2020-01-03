28e55094bff33b63bfc7a70eea47e43fe24ef605
Alan Delimon <alan.delimon@ellucian.com>
2020-01-03 14:44:30 -0500

    Filter pending applications, and added email wording

--- src/main/java/com/ajdconsulting/pra/clubmanager/repository/MemberRepository.java
+++ src/main/java/com/ajdconsulting/pra/clubmanager/repository/MemberRepository.java
@@ -25,6 +25,6 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
     @Query("select m from Member m order by lastName")
     public List<Member> findAllMembersOrderByLastName();
 
-    @Query("select m from Member m where endDate is null and m.status.type != 'Paid Labor' order by lastName, firstName")
+    @Query("select m from Member m where endDate is null and m.status.type != 'Paid Labor' and m.status.type != 'Application Pending' order by lastName, firstName")
     public List<Member> findBillableMembers();
 }
