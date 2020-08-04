0ece5035c09f7ebf280bf3a64e7e52ad55496f0e
Alan Delimon <alan.delimon@ellucian.com>
2020-08-04 08:50:16 -0400

    a few more quality of life improvements to make sorting the sme across all pages and also added clarifying text to the fields.  This needs a re-imaginging most likely but that's later..

--- src/main/java/com/ajdconsulting/pra/clubmanager/repository/JobRepository.java
+++ src/main/java/com/ajdconsulting/pra/clubmanager/repository/JobRepository.java
@@ -24,13 +24,13 @@ public interface JobRepository extends JpaRepository<Job,Long> {
     public Page<Job> findPaidJobs(Pageable pageable);
 
     @Query("select j from Job j where j.online=true and j.eventType.id = :eventTypeId and j.id not in "+
-        "(select s.job.id from Signup s where s.scheduleDate.id = :scheduleDateId) order by j.title")
+        "(select s.job.id from Signup s where s.scheduleDate.id = :scheduleDateId) order by j.jobDayNumber, j.sortOrder")
     public Page<Job> findOpenJobsForDate(Pageable pageable,
         @Param("eventTypeId") Long eventTypeId,
         @Param("scheduleDateId") Long scheduleDateId);
 
     @Query("select j from Job j where j.online=true and j.eventType.id = :eventTypeId  and j.reserved = false and j.id not in "+
-        "(select s.job.id from Signup s where s.scheduleDate.id = :scheduleDateId) order by j.title")
+        "(select s.job.id from Signup s where s.scheduleDate.id = :scheduleDateId) order by j.jobDayNumber, j.sortOrder")
     public Page<Job> findOpenJobsForDateNoReserved(Pageable pageable,
                                          @Param("eventTypeId") Long eventTypeId,
                                          @Param("scheduleDateId") Long scheduleDateId);
